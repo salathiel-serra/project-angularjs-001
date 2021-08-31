db0dba154ca68da189a7d388db8a267ebf6825bb
thiel.serra@gmail.com <thiel.serra@gmail.com>
2021-08-30 21:58:13 -0300

    Configurando angular-route e definindo rotas parra testes

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,14 @@
-angular.module("myModule",[]);
+angular.module("myModule",["ngRoute"])
+.config(function($routeProvider){
+	$routeProvider
+	.when("/home",{
+		templateUrl:"templates/home.html",
+		controller:"indexController"
+	})
+	.when("/contact",{
+		templateUrl:"templates/contact.html"
+	});
+
+	$routeProvider.otherwise({redirectTo:"/home"});
+
+})
